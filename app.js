90d32abd7e5fb43a1015d3e29e3b356801812681
Maxime Pawlak <contact@maximepawlak.fr>
2016-10-04 09:56:42 +0200

    Add step 1

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 	//votre code
+	console.log('qq bonjour !');
+	var counter = 0;
+
+	$('.counter span').click(function(){
+		counter++;
+		$(this).text(counter);
+	});
 
 
 });
