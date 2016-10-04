2ac54cdc7ed80a0ac8ebb14eeea1446c1bcf4a7e
Maxime Pawlak <contact@maximepawlak.fr>
2016-10-04 10:04:57 +0200

    Add Bonus 3

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
 	console.log('qq bonjour !');
 	var counter = 0;
 
+	var messages = [
+		'Coucou, vite !',
+		'Hey ! Ca va ?',
+		'Dur dur d\'avoir de l\'imagination...',
+		'Tu fais quoi ce soir ?',
+		'lorem bidule bidule',
+		'1998, la France est championne du moooooooooooonde !'
+	];
+
 	$('.counter span').click(function(){
 		counter++;
 		$(this).text(counter);
@@ -10,8 +19,15 @@ $(document).ready(function(){
 		var date = new Date().toISOString();
 		var mod20 = counter%3;
 		if(mod20 == 0){
-			alert('Tu as cliqué ' + counter + ' fois. Il est ' +
-				date + ', tu n\'as rien d\'autre à faire ?');
+			// Random number between 0 and 1
+			var aleatoire01 = Math.random();
+			
+			var len = messages.length;
+			// multiply to get a number between 0 and messages.length
+			var indexAleatoire = parseInt(aleatoire01 * len, 10);
+			
+			var msg = messages[indexAleatoire];
+			alert(msg);
 		}
 	});
 
