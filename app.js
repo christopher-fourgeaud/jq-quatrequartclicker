175982a2c1b5dcf2372159c7e894626b38084b28
Maxime Pawlak <contact@maximepawlak.fr>
2016-10-04 09:58:24 +0200

    Add alert every 20 click.

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
 	$('.counter span').click(function(){
 		counter++;
 		$(this).text(counter);
+
+		var mod20 = counter%3;
+		if(mod20 == 0){
+			alert('Trouve une activité plus productive et va lire Eloquent Js');
+		}
 	});
 
 
