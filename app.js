96353cdca09d592fafa7f296fdabf0f694c191cb
mounga <christopher.fourgeaud@gmail.com>
2017-08-01 12:25:41 +0200

    Cookie clicker

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+
+
 $(document).ready(function(){
-	//votre code
+	var compteur= 0;
+	var tableau=["Fait un jeu de dames","Fait un jeu d'echec","Fait Starcraft 3"]
+
+	
+
+	$(".cake-is-a-lie").click(function(){
+
+		compteur++;
+
+		$(".counter").html(compteur);
+
+		var index = Math.floor(Math.random() * 3);
+
+		
+		if (compteur%20 === 0) {
+
+			alert(tableau[index]);
+		}
 
 
+	});
+	
 });
+
